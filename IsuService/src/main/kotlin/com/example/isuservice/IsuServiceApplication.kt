2a120d7f30094529e519ed3fa501e3c49a09c84e
nt1dc <nt1dcare@gmail.com>
2023-10-28 17:19:57 +0300

    anal

--- IsuService/src/main/kotlin/com/example/isuservice/IsuServiceApplication.kt
+++ IsuService/src/main/kotlin/com/example/isuservice/IsuServiceApplication.kt
@@ -8,13 +8,17 @@ import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuil
 import org.apache.hc.client5.http.io.HttpClientConnectionManager
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory
 import org.apache.hc.core5.ssl.SSLContextBuilder
+import org.apache.hc.core5.ssl.SSLContexts
+import org.apache.hc.core5.ssl.TrustStrategy
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestFactory
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import java.security.cert.X509Certificate
 import javax.net.ssl.SSLContext
 
 
@@ -31,8 +35,11 @@ class RestConf() {
 
     @Bean
     fun restTemplate(): RestTemplate {
-        val sslContext: SSLContext = SSLContextBuilder()
-            .loadTrustMaterial(RestConf::class.java.classLoader.getResource("keystore.p12"), "12345678".toCharArray()).build()
+        val acceptingTrustStrategy =
+            TrustStrategy { _: Array<X509Certificate?>?, _: String? -> true }
+        val sslContext: SSLContext = SSLContexts.custom()
+            .loadTrustMaterial(null, acceptingTrustStrategy)
+            .build()
         val sslConFactory = SSLConnectionSocketFactory(sslContext)
         val cm: HttpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder.create()
             .setSSLSocketFactory(sslConFactory)
