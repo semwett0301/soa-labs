695b65701a8dacf216b329d79e6b4710c8feaefc
nt1dc <nt1dcare@gmail.com>
2023-10-28 17:28:34 +0300

    sosi zhopu

--- IsuService/src/main/kotlin/com/example/isuservice/IsuServiceApplication.kt
+++ IsuService/src/main/kotlin/com/example/isuservice/IsuServiceApplication.kt
@@ -6,12 +6,11 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
 import org.apache.hc.client5.http.impl.classic.HttpClients
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
 import org.apache.hc.client5.http.io.HttpClientConnectionManager
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory
-import org.apache.hc.core5.ssl.SSLContextBuilder
 import org.apache.hc.core5.ssl.SSLContexts
 import org.apache.hc.core5.ssl.TrustStrategy
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -40,7 +39,7 @@ class RestConf() {
         val sslContext: SSLContext = SSLContexts.custom()
             .loadTrustMaterial(null, acceptingTrustStrategy)
             .build()
-        val sslConFactory = SSLConnectionSocketFactory(sslContext)
+        val sslConFactory = SSLConnectionSocketFactory(sslContext,NoopHostnameVerifier())
         val cm: HttpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder.create()
             .setSSLSocketFactory(sslConFactory)
             .build();
