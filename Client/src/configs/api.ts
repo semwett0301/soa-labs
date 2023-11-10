a11a53de727bd301bfdf95976cdc2cf4c24747ca
semen.mokrov <semen.mokrov@gmail.com>
2023-11-10 21:24:35 +0300

    fix: made Gateway work again

--- Client/src/configs/api.ts
+++ Client/src/configs/api.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const BASE_URL = "https://localhost:443/api/v1";
+export const BASE_URL = "https://localhost:8761/api/v1";
 
 export const BASE_GROUP_API = fetchBaseQuery({
   baseUrl: BASE_URL,
