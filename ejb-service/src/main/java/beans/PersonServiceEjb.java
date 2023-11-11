80725b69086481b8cc46c55970d9b848b4d6fa1e
semen.mokrov <semen.mokrov@gmail.com>
2023-11-11 15:07:15 +0300

    feat: add pooling to EJB

--- ejb-service/src/main/java/beans/PersonServiceEjb.java
+++ ejb-service/src/main/java/beans/PersonServiceEjb.java
@@ -5,12 +5,14 @@ import entity.Person;
 import interfaces.PersonService;
 import jakarta.ejb.Remote;
 import jakarta.ejb.Stateless;
+import org.jboss.ejb3.annotation.Pool;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Remote(PersonService.class)
 @Stateless(name = "PersonServiceEjb")
+@Pool("slsb-strict-max-pool")
 public class PersonServiceEjb implements PersonService {
     @Override
     public List<Person> getAll() {
