7c971258a217f2040d6aa7b4914149908c034c30
nt1dc <nt1dcare@gmail.com>
2023-12-23 04:49:50 +0300

    feat: soap server

--- isu-sevice-maven/src/main/java/com/example/isusevicemaven/BestMapperEver.java
+++ isu-sevice-maven/src/main/java/com/example/isusevicemaven/BestMapperEver.java
@@ -1,6 +1,8 @@
 package com.example.isusevicemaven;
 
+import io.spring.guides.gs_producing_web_service.FormOfEducation;
 import io.spring.guides.gs_producing_web_service.Person;
+import io.spring.guides.gs_producing_web_service.Semester;
 import io.spring.guides.gs_producing_web_service.StudyGroup;
 
 import javax.xml.datatype.DatatypeConfigurationException;
@@ -19,7 +21,9 @@ public class BestMapperEver {
         studyGroup.setStudentsCount(studyGroupEntity.getStudentsCount());
         studyGroup.setName(studyGroupEntity.getName());
         studyGroup.setCreationDate(localDateTimeto(studyGroupEntity.getCreationDate()));
-
+        studyGroup.setFormOfEducation(FormOfEducation.valueOf(studyGroupEntity.getFormOfEducation().name()));
+        studyGroup.setSemesterEnum(Semester.valueOf(studyGroupEntity.getSemesterEnum().name()));
+        studyGroup.setGroupAdmin(entityToSoap(studyGroupEntity.getGroupAdmin()));
         return studyGroup;
     }
 
@@ -29,10 +33,14 @@ public class BestMapperEver {
         return DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
     }
 
-    private static Person entityToSoap(entity.Person entity) {
+    private static Person entityToSoap(entity.Person entity) throws DatatypeConfigurationException {
         Person person = new Person();
         person.setId(entity.getId());
         person.setName(entity.getName());
+        person.setPassportID(entity.getPassportID());
+        person.setHeight(entity.getHeight());
+        person.setWeight(entity.getWeight());
+        person.setBirthday(localDateTimeto(LocalDate.from(entity.getBirthday())));
         return person;
     }
 }
--- isu-sevice-maven/src/main/java/com/example/isusevicemaven/IsuEndpoint.java
+++ isu-sevice-maven/src/main/java/com/example/isusevicemaven/IsuEndpoint.java
@@ -2,8 +2,12 @@ package com.example.isusevicemaven;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.spring.guides.gs_producing_web_service.*;
+import entity.FormOfEducation;
+import entity.StudyGroup;
+import io.spring.guides.gs_producing_web_service.ChangeEduFormRequest;
+import io.spring.guides.gs_producing_web_service.ChangeEduFormResponse;
+import io.spring.guides.gs_producing_web_service.ExpelAllRequest;
+import io.spring.guides.gs_producing_web_service.ExpelAllResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -29,15 +33,16 @@ public class IsuEndpoint {
         ExpelAllResponse expelAllResponse = new ExpelAllResponse();
         expelAllResponse.setStudyGroup(BestMapperEver.from(studyGroup1));
         return expelAllResponse;
-
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "changeEduFormRequest")
     @ResponsePayload
-    public ChangeEduFormResponse changeEduForm(@RequestPayload ChangeEduFormRequest request) {
+    public ChangeEduFormResponse changeEduForm(@RequestPayload ChangeEduFormRequest request) throws JsonProcessingException, DatatypeConfigurationException {
+        entity.StudyGroup studyGroup = studyGroupApi.studyGroupById(request.getId());
+        studyGroup.setFormOfEducation(FormOfEducation.valueOf(request.getNewFormOfEducation().name()));
+        StudyGroup studyGroup1 = studyGroupApi.putStudyGroup(studyGroup);
         ChangeEduFormResponse response = new ChangeEduFormResponse();
-        StudyGroup studyGroup = new StudyGroup();
-        response.setStudyGroup(studyGroup);
+        response.setStudyGroup(BestMapperEver.from(studyGroup1));
         return response;
     }
 
